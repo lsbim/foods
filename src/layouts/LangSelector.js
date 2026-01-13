7f58be244d85d455c45482144761b440897ffe24
lsbim <rnqjatnqja@gmail.com>
2026-01-13 21:37:09 +0900

    추가: 선택지 강조

--- src/layouts/LangSelector.js
+++ src/layouts/LangSelector.js
@@ -5,7 +5,7 @@ const LangSelector = () => {
 
     const [isOn, setIsOn] = useState(false);
     const dropdownRef = useRef(null);
-    const { setLanguage } = useLanguage();
+    const { setLanguage, language } = useLanguage();
 
     useEffect(() => {
         const handleClickOutside = (event) => {
@@ -41,7 +41,7 @@ const LangSelector = () => {
         <div
             ref={dropdownRef}
             onClick={() => setIsOn(!isOn)}
-            className="relative z-40">
+            className="relative z-40 select-none">
             <div className={`flex p-2 items-center cursor-pointer hover:bg-zinc-100 rounded-md transition duration-150 ${isOn && 'bg-zinc-100'}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802" />
@@ -49,13 +49,20 @@ const LangSelector = () => {
                 <span className="text-[12px] sm:inline hidden">Lang</span>
             </div>
             {isOn && (
-                <div className="flex flex-col gap-y-2 absolute top-10 sm:left-0 right-0 w-[100px] shadow-md bg-white">
+                <div className="flex flex-col absolute top-10 sm:left-0 right-0 w-[100px] shadow-md bg-white">
                     {langList.map(li => (
                         <div
                             onClick={() => handleLangChange(li.lang)}
-                            className="hover:bg-zinc-200 cursor-pointer p-2 "
+                            className={`hover:bg-zinc-200 cursor-pointer p-2 py-3 relative flex items-center justify-between ${language === li.lang && 'font-bold'}`}
                             key={'lang_' + li.label}>
-                            {li.label}
+                            <span>
+                                {li.label}
+                            </span>
+                            {language === li.lang && (
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
+                                    <path fillRule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd" />
+                                </svg>
+                            )}
                         </div>
                     ))}
                 </div>
--- src/layouts/ServerSelector.js
+++ src/layouts/ServerSelector.js
@@ -5,7 +5,7 @@ const ServerSelector = () => {
 
     const [isOn, setIsOn] = useState(false);
     const dropdownRef = useRef(null);
-    const { setServer } = useLanguage();
+    const { setServer, server } = useLanguage();
 
     useEffect(() => {
         const handleClickOutside = (event) => {
@@ -27,8 +27,8 @@ const ServerSelector = () => {
 
     // 繁體中文 번체중문 미구현
     const langList = [
-        { label: 'Kr', lang: 'kr' },
-        { label: 'Global', lang: 'global' },
+        { label: 'Kr', server: 'kr' },
+        { label: 'Global', server: 'global' },
     ]
 
     const handleLangChange = (lang) => {
@@ -40,7 +40,7 @@ const ServerSelector = () => {
         <div
             ref={dropdownRef}
             onClick={() => setIsOn(!isOn)}
-            className="relative z-50">
+            className="relative z-50 select-none">
             <div className={`flex p-2 items-center cursor-pointer hover:bg-zinc-100 rounded-md transition duration-150 ${isOn && 'bg-zinc-100'}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 0 1-1.161.886l-.143.048a1.107 1.107 0 0 0-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 0 1-1.652.928l-.679-.906a1.125 1.125 0 0 0-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 0 0-8.862 12.872M12.75 3.031a9 9 0 0 1 6.69 14.036m0 0-.177-.529A2.25 2.25 0 0 0 17.128 15H16.5l-.324-.324a1.453 1.453 0 0 0-2.328.377l-.036.073a1.586 1.586 0 0 1-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 0 1-5.276 3.67m0 0a9 9 0 0 1-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />
@@ -48,13 +48,20 @@ const ServerSelector = () => {
                 <span className="text-[12px] sm:inline hidden">Server</span>
             </div>
             {isOn && (
-                <div className="flex flex-col gap-y-2 absolute top-10 sm:left-0 right-0 w-[100px] shadow-md bg-white">
+                <div className="flex flex-col absolute top-10 sm:left-0 right-0 w-[100px] shadow-md bg-white">
                     {langList.map(li => (
                         <div
-                            onClick={() => handleLangChange(li.lang)}
-                            className="hover:bg-zinc-200 cursor-pointer p-2 "
+                            onClick={() => handleLangChange(li.server)}
+                            className={`hover:bg-zinc-200 cursor-pointer p-2 py-3 relative flex items-center justify-between ${server === li.server && 'font-bold'}`}
                             key={'lang_' + li.label}>
-                            {li.label}
+                            <span>
+                                {li.label}
+                            </span>
+                            {server === li.server && (
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
+                                    <path fillRule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd" />
+                                </svg>
+                            )}
                         </div>
                     ))}
                 </div>
